f2881caaefa772155284553f1dd4814e7a68d784
Zoe <za.zoeang@gmail.com>
2024-03-29 22:00:11 +0800

    Fix Test Case for EditCommandTest

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -5,8 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_BIRTHDAY_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ROOMNUMBER_AMY_W_DATE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TELEGRAM_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
@@ -35,20 +39,24 @@ import seedu.address.testutil.PersonBuilder;
 public class EditCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-    // @Test
-    // public void execute_allFieldsSpecifiedUnfilteredList_success() {
-    //     Person editedPerson = new PersonBuilder().build();
-    //     EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
-    //     EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, descriptor);
+    @Test
+    public void execute_allFieldsSpecifiedUnfilteredList_success() {
+        Person editedPerson = new PersonBuilder()
+                .withEmail(VALID_EMAIL_AMY)
+                .withRoomNumber(VALID_ROOMNUMBER_AMY_W_DATE)
+                .withTelegram(VALID_TELEGRAM_AMY)
+                .withBirthday(VALID_BIRTHDAY_AMY).build();
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, descriptor);
 
-    //     String expectedMessage =
-    //             String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
+        String expectedMessage =
+                String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
 
-    //     Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-    //     expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
 
-    //     assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-    // }
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
 
     @Test
     public void execute_someFieldsSpecifiedUnfilteredList_success() {
