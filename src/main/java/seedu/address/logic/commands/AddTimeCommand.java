30afcf4986f3efe77d81ca305d8b8e38f83dca9e
larainezo <larainelwx@gmail.com>
2024-04-01 20:53:55 +0800

    Fix code style

--- src/main/java/seedu/address/logic/commands/AddTimeCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTimeCommand.java
@@ -1,8 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DORMTAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FREETIMETAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.HashSet;
