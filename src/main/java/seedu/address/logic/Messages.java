e968b24abd168472940d3dbf200836e11276538b
PallonCX <cheexiang03@gmail.com>
2024-04-12 16:44:07 +0800

    Edit welcome message

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,9 +14,8 @@ public class Messages {
 
     public static final String MESSAGE_WELCOME =
             "Welcome to Dormie!\n"
-                    + "The available commands are as follows: list, add, edit, delete, addTime, deleteTime"
-                    + ", whoisFree, find, "
-                    + "clear, exit\n"
+                    + "The available commands are as follows: add, addTime, clear, delete, deleteTime, edit, exit, "
+                    + "find, help, list, whoisfree.\n"
                     + "Refer to the help guide (link can be found above) for more details.";
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
